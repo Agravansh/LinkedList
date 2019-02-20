060263d509ab0ff79860abeda7c0e345d728bfda
Agravansh <kapilagrawal341997@gmail.com>
2019-02-20 11:16:26 +0530

    push commit

--- src/Main/Node.java
+++ src/Main/Node.java
@@ -1,4 +1,15 @@
 package Main;
 
 public class Node {
+    //commit- ctrl+K
+    // push ctrl+shift+k
+
+private int data ;
+private Node next;
+public int getData(){
+    return data ;
+}
+public void getData(int data){
+    this.data = data;
+}
 }
